13e313ec940b07fb8ce331f83b4ce1ab5bde21dc
Sandruez <candrapapr1501@gmail.com>
2026-03-23 20:02:43 +0530

    resolving deployment essues

--- frontend/vite.config.dev.js
+++ frontend/vite.config.dev.js
@@ -8,7 +8,19 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target:  process.env.VITE_API_URL,
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+      }
+    }
+  },
+  // ✅ This is what Railway needs
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: 'all',
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
       }
     }
